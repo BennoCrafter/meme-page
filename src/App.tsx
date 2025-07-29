328b9bfcbead13738769f5b9029de51849dbe42c
Benno <107504783+BennoCrafter@users.noreply.github.com>
2025-07-29 12:42:25 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -20,7 +20,7 @@ function App() {
       {/* Video Player Start */}
       <div style={{ margin: '2rem 0' }}>
         <video width="480" controls>
-          <source src="assets/ralf-schuhmacher.mp4" type="video/mp4" />
+          <source src="src/assets/ralf-schuhmacher.mp4" type="video/mp4" />
           Your browser does not support the video tag.
         </video>
       </div>
