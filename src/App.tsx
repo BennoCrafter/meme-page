09acfeb14bc87edfe550160fbbaf30b88fa803c0
Benno <107504783+BennoCrafter@users.noreply.github.com>
2025-07-29 12:44:08 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import viteLogo from "./assets/vite.svg";
+import ralfSchuhmacher from "./assets/ralf-schuhmacher.mp4";
 import "./App.css";
 
 function App() {
@@ -8,34 +9,14 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
       {/* Video Player Start */}
       <div style={{ margin: '2rem 0' }}>
         <video width="480" controls>
-          <source src="src/assets/ralf-schuhmacher.mp4" type="video/mp4" />
+          <source src={ralfSchuhmacher} type="video/mp4" />
           Your browser does not support the video tag.
         </video>
       </div>
-      {/* Video Player End */}
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      {/* Video Player End */}  
     </>
   );
 }
