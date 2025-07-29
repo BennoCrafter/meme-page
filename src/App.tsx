c68bd4a7f615f60053874fb5c08393c66ec786b9
Benno <107504783+BennoCrafter@users.noreply.github.com>
2025-07-29 13:38:06 +0200

    Update page title and add package-lock.json
    Changed the HTML page title from 'Vite + React + TS' to 'Meme Page' in index.html. Added package-lock.json to track project dependencies.

--- src/App.tsx
+++ src/App.tsx
@@ -2,17 +2,20 @@ import ralfSchuhmacher from "./assets/ralf-schuhmacher.mp4";
 import "./App.css";
 
 function App() {
-
   return (
     <>
       {/* Video Player Start */}
-      <div style={{ margin: '2rem 0' }}>
-        <video width="480" controls>
+      <div style={{ margin: "2rem 0" }}>
+        <video
+          style={{ maxWidth: "100%", height: "auto" }}
+          controls
+          playsInline
+        >
           <source src={ralfSchuhmacher} type="video/mp4" />
           Your browser does not support the video tag.
         </video>
       </div>
-      {/* Video Player End */}  
+      {/* Video Player End */}
     </>
   );
 }
